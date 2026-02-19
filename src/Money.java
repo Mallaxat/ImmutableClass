fc5841b3105ce373da3eec773bb734c0813d8d40
Mallaxat <alina-lapkina@mail.ru>
2026-02-19 15:02:54 +0300

    Update Money.java

--- src/Money.java
+++ src/Money.java
@@ -1,4 +1,36 @@
 package PACKAGE_NAME;
 
-public class Money {
+public final class Money
+{
+  private final int money;              //Будет хранить кол-во денег
+  private final ArrayList<String> curr; //Будет хранить в себе значение валюты
+
+
+//Конструктор с параметрами с полным заполнением полей всех переменных
+    public Money(int money,ArrayList<String> array)
+    {
+        this.money=money;
+        //Копируем исходный массив
+        this.curr=new ArrayList<>(array);
+
+    }
+    //Геттер для возвращения суммы денег
+    public int getMoney()
+    {
+        return money;
+    }
+
+    //Возвращаем копию массива, хранящего валюты
+    public ArrayList<String> getCurr()
+    {
+        return new ArrayList<String> (curr);
+    }
+    //Этот геттер создал для подтверждения и проверки, что в результате на самом деле будет отличный объект
+    //В реализации он должен быть удален
+    public ArrayList<String> getCurrDelete()
+    {
+        return curr;
+    }
+
 }
+
