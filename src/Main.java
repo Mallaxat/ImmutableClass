493d31a5edfd566ea16bfcf00b37c3e10b8daf2a
Mallaxat <alina-lapkina@mail.ru>
2026-02-19 15:01:21 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+ /*     Реализовать свой иммутабельный класс,
+        который будет внутри себя содержать поле с изменяемым классом*/
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int x=500;
+        ArrayList<String>test =new  ArrayList<String>();
+        ArrayList<String>test2 =new  ArrayList<String>();
+        test.add("Евро");
+        test.add("Рубли");
+        test.add("Доллары");
+
+        Money One=new Money(x,test);
+        //Возвращаем копию массива
+        test2=One.getCurr();
+        //Здесь сравнение с ВРЕМЕННЫМ методом, для подтверждения, что реально создалась копия
+        boolean tests=test2==One.getCurrDelete();
+
+        System.out.println("Результат проверки ссылок 2х объектов: "+tests);
+        System.out.println("Содержимое test 2: " + test2);
+        System.out.println("Результат возвращения переменной money: "+One.getMoney());
+
+        tests=One.getCurr() == One.getCurr();
+        System.out.println("Результат проверки ссылок 2х копий объектов: "+tests);
+
+
         }
     }
 }
